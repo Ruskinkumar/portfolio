4c23b93349aa5dcbe6c0e39cab3b0fee2f4a8b64
ruskin <ruskinkumar24@gmail.com.com>
2024-09-08 15:05:39 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Header />
       <Router>
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
           {/* Add other routes here if needed */}
         </Routes>
       </Router>
