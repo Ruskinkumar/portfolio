d9068e5f5280f724a6351390d0b7a89b0cae9041
ruskin <ruskinkumar24@gmail.com.com>
2024-09-08 14:54:17 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import React from 'react'
-import Home from './pages/Home'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react';
+import Home from './pages/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div>
       <Header />
-    <BrowserRouter>
-    
-    <Routes>
-
-   <Route path='/' element={<Home/>} />
-
-
-    </Routes>
-    
-
-
-    </BrowserRouter>
-    <Footer />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          {/* Add other routes here if needed */}
+        </Routes>
+      </Router>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
